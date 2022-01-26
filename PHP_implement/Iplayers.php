f773445e47ef25a775268575b07d5cc06e6e5830
Anhao <jiaoa3@mcmaster.ca>
2022-01-26 10:46:08 -0500

    remove unwanted file

--- PHP_implement/Iplayers.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-interface IReadWritePlayers {
-    function readPlayers($source, $filename = null);
-    function writePlayer($source, $player, $filename = null);
-    function display($isCLI, $course, $filename = null);
-}
-
-?>
