c5fb0613e88c015777efa913b50b7c9c1a6cc2aa
Anhao <jiaoa3@mcmaster.ca>
2022-01-28 11:15:00 -0500

    delete repeated func

--- PHP_implement/View/CLIView.php
+++ PHP_implement/View/CLIView.php
@@ -2,9 +2,16 @@
 
 class CLIView implements IPlayersView
 {
-
     function display($playerCollection)
     {
-        // TODO: Implement display() method.
+        $players = []; // need to change later
+        // TODO something
+        echo "Current Players: \n";
+        foreach ($players as $player) {
+            echo "\tName: $player->name\n";
+            echo "\tAge: $player->age\n";
+            echo "\tSalary: $player->salary\n";
+            echo "\tJob: $player->job\n\n";
+        }
     }
 }
